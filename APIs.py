461c61e1e998effa0a7e94f45950911f65d5d45c
LuanBoheng <314887027@qq.com>
2018-09-28 09:35:05 +0000

    signup, login work!

--- APIs.py
+++ APIs.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, Response
 from flask_restful import Resource, Api
 from sqlalchemy import create_engine
 from json import dumps
@@ -7,6 +7,7 @@ import json
 import pymongo  # pip install pymongo
 from bson import json_util 
 from pymongo import MongoClient# Comes with pymongo
+import os
 
 print('package loaded!')
 
@@ -35,17 +36,37 @@ def GET(collection, query = None):
 
 class Session_token:
     def __init__(self):
+        self.user2token = dict()
+        self.token2user = dict()
+    
+    def new_session(self, email):
+        token = str(os.urandom(32))
+        self.user2token[email] = token
+        self.token2user[token] = email
+        return token
 
+    def is_session(self, token):
+        return token in self.token2user
+
+token_cache = Session_token()
 
 class Test(Resource):
     def get(self, arg):
         return json.dumps({'message':'connection established!', 'arg':arg})
 
 class Login(Resource):
-    def get(self, user_name, password): # for login function
-        print('#debug print:', (user_name, password))
-        user = GET('user_profile', {'user':user_name, 'password':password})
-        return 
+    def get(self, email, password): # for login function
+        print('#debug print:', (email, password))
+        query =  {'email':email, 'password':password}
+        user = GET('user_profile', query)
+        if user['length'] == 0:
+            message = {'message':'incorrect email or password'}
+        else:
+            token = str(token_cache.new_session(email))
+            message = {'message':'login successful! ' + token}
+        resp = Response(json.dumps(message))
+        resp.headers['Access-Control-Allow-Origin'] = '*'
+        return resp 
 
 class Sign_up(Resource):
     def get(self, user_name, password, email):
@@ -54,19 +75,22 @@ class Sign_up(Resource):
         user_exist = GET('user_profile',user)['length'] != 0
         print('#debug print: user exist', GET('user_profile',user), user_exist) 
         if user_exist:
-            return 'user already exist!'
+            message = json.dumps({'message':'user already exist!'})
         else:
             POST('user_profile',user)
-            return 'sign up successful!'
-
+            message = json.dumps({'message':'sign up successful!'})
+        resp = Response(message)
+        resp.headers['Access-Control-Allow-Origin'] = '*'
+        return resp
 
 app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(Test,'/test/<arg>')
-api.add_resource(Login,'/login/<user_name>/<password>')
+api.add_resource(Login,'/login/<email>/<password>')
 api.add_resource(Sign_up,'/signup/<user_name>/<password>/<email>')
 
 if __name__ == "__main__":
-    app.run('115.146.92.114',port=8888)
+    #app.run('115.146.92.114', port=8888, ssl_context='adhoc')
+    app.run('115.146.92.114', port=8888)
 
